21479bc4dc2402c30ee04ee47d07d7ba2223a34f
Cameron Nelson <cjnelson2004@gmail.com>
2020-12-09 13:13:21 -0500

    NoNull set up, init commit for OrderedArrayList

---
+++ NoNullArrayList.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class NoNullArrayList<T> extends ArrayList<T> {
+
+  public NoNullArrayList() {
+    super();
+  }
+  public NoNullArrayList(int initCapacity) {
+    super(initCapacity);
+  }
+  public boolean add(T e) {
+    if (e == null) {throw new IllegalArgumentException("Illegal argument 'null'");}
+    else {
+      return super.add(e);
+    }
+  }
+  public void add(int index,T e) {
+    if (e == null) {throw new IllegalArgumentException("Illegal argument 'null'");}
+    else {
+      super.add(index,e);
+    }
+  }
+  public T set(int index,T e) {
+    if (e == null) {throw new IllegalArgumentException("Illegal argument 'null'");}
+    else {
+      return super.set(index,e);
+    }
+  }
+}
---
+++ OrderedArrayList.java
@@ -0,0 +1,8 @@
+public class OrderedArrayList<T> extends Comparable<T>, NoNullArrayList<T> {
+  public OrderedArrayList() {
+    super();
+  }
+  public OrderedArrayList(initCapacity) {
+    super(initCapacity);
+  }
+}
