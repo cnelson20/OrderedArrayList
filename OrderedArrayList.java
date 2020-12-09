efbfa1c4b47e8a2b7c2e356b4c53d7e0d66d3426
Cameron Nelson <cjnelson2004@gmail.com>
2020-12-09 13:39:39 -0500

    OrderedArrayList works now!

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,8 +1,25 @@
-public class OrderedArrayList<T> extends Comparable<T>, NoNullArrayList<T> {
+import java.util.*;
+public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T> {
   public OrderedArrayList() {
     super();
   }
-  public OrderedArrayList(initCapacity) {
+  public OrderedArrayList(int initCapacity) {
     super(initCapacity);
+  }
+  public boolean add(T e) {
+    int i = 0;
+    while (i < super.size() && e.compareTo(super.get(i)) > 0) {
+      i++;
+    }
+    super.add(i,e);
+    return true;
+  }
+  public void add(int index,T e) {
+    this.add(e);
+  }
+  public T set(int index,T e) {
+    super.remove(index);
+    this.add(e);
+    return e;
   }
 }
