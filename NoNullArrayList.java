3ef19b71c25f45d96c535bea13f00ce1bf25264a
cnelson20 <cjnelson2004@gmail.com>
2020-12-14 00:25:53 -0500

    the methods throw errors now!

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -7,19 +7,19 @@ public class NoNullArrayList<T> extends ArrayList<T> {
   public NoNullArrayList(int initCapacity) {
     super(initCapacity);
   }
-  public boolean add(T e) {
+  public boolean add(T e) throws IllegalArgumentException {
     if (e == null) {throw new IllegalArgumentException("Illegal argument 'null'");}
     else {
       return super.add(e);
     }
   }
-  public void add(int index,T e) {
+  public void add(int index,T e) throws IllegalArgumentException {
     if (e == null) {throw new IllegalArgumentException("Illegal argument 'null'");}
     else {
       super.add(index,e);
     }
   }
-  public T set(int index,T e) {
+  public T set(int index,T e) throws IllegalArgumentException {
     if (e == null) {throw new IllegalArgumentException("Illegal argument 'null'");}
     else {
       return super.set(index,e);
--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -6,20 +6,27 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   public OrderedArrayList(int initCapacity) {
     super(initCapacity);
   }
-  public boolean add(T e) {
-    int i = 0;
-    while (i < super.size() && e.compareTo(super.get(i)) > 0) {
-      i++;
+  public boolean add(T e) throws IllegalArgumentException {
+    if (e == null) {throw new IllegalArgumentException("Illegal argument 'null'");}
+    else {
+      int i = 0;
+      while (i < super.size() && e.compareTo(super.get(i)) > 0) {
+        i++;
+      }
+      super.add(i,e);
+      return true;
     }
-    super.add(i,e);
-    return true;
   }
-  public void add(int index,T e) {
+  public void add(int index,T e) throws IllegalArgumentException {
     this.add(e);
   }
-  public T set(int index,T e) {
-    super.remove(index);
-    this.add(e);
-    return e;
+  public T set(int index,T e) throws IllegalArgumentException {
+    if (e == null) {throw new IllegalArgumentException("Illegal argument 'null'");}
+    else {
+      T s = super.get(index);
+      super.remove(index);
+      this.add(e);
+      return s;
+    }
   }
 }
